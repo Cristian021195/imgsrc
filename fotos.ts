3695bffcd01151ba6650e5af984047a5b694b960
unknown <cristian021195>
2023-04-27 23:57:35 -0300

    cambios

--- fotos.ts
+++ fotos.ts
@@ -6,7 +6,7 @@ let img2 = "/images/projects/horabondi/main.jpg";
 export const projects = [img1, img2];*/
 
 import img1 from "./public/images/projects/horabondi/contacto.jpg";
-import img2 from "./public/images/projects/horabondi/MAIN.jpg";
+import img2 from "./public/images/projects/horabondi/main.jpg";
 //import img2 from "../.. /images/main.jpg";
 //import img1 from " /images/projects/horabondi/contacto.jpg";
 //import img2 from " /images/projects/horabondi/main.jpg";
